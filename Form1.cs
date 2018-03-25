011bf3dda5a0d5657147eaaf2f4ba7310fa20439
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-24 20:15:20 -0400

    Added the int variables I wanted to use.

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,11 @@ namespace TestApp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int input1, input2, answer;
+
+        }
     }
 }
